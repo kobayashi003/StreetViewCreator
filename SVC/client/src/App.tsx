a82305edd03fe67410307da342d909b4dfcef8ed
moriya-24 <166358365+moriya-24@users.noreply.github.com>
2024-04-25 21:32:15 +0900

    devide URL

--- SVC/client/src/App.tsx
+++ SVC/client/src/App.tsx
@@ -1,38 +1,21 @@
-import React, { useState, ChangeEvent, FormEvent } from 'react';
-import axios from 'axios';
-import "./css/test.css"  //cssの導入例
-import Communication from "./component/Communication" //componentの導入例
-import Title from './component/Title';
-import { BrowserRouter as Router, Route,Routes, Link } from 'react-router-dom';
-// import React from 'react';
-import ReactDOM from 'react-dom';
-// import { BrowserRouter, Routes, Route } from 'react-router-dom';
-// import App from './App';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import APIComponent from './component/APIComponent';  // /api パスで表示するコンポーネント
+import Edit from './Edit';
+import User from './User';
+
 
 const App: React.FC = () => {
   return (
-    <div>
-      <div>
-        <Title />
-      </div>
-      <div>
-        <Communication />
-      </div>
-
-    </div>
+    <Router>
+      <Routes>
+      {/* <Route path="/api" element={<APIComponent />} /> */}
+        <Route path="/edit" element={<Edit />} />
+        <Route path="/user" element={<User />} />
+        {/* Define routes for other components if needed */}
+      </Routes>
+    </Router>
   );
 };
 
 export default App;
-
-
-ReactDOM.render(
-  <Router>
-    <Routes>
-      <Route path="/" element={<App />} />
-      <Route path="/api" element={<APIComponent />} />
-    </Routes>
-  </Router>,
-  document.getElementById('root')
-);
